046a50d94d704115720e9a426eb7cc6037cde202
vaibhav <singhvaibhav396@gmail.com>
2020-05-15 03:20:22 +0530

    0.918

---
+++ client/build/precache-manifest.c12d4b7c5cc0bbfda590319de8bd6f2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c5277e0ca736425cf3b",
+    "url": "/static/css/main.0fb68707.chunk.css"
+  },
+  {
+    "revision": "4c5277e0ca736425cf3b",
+    "url": "/static/js/main.4c5277e0.chunk.js"
+  },
+  {
+    "revision": "6bcab7b79ebc27752533",
+    "url": "/static/js/1.6bcab7b7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09e5e1cccf6ba538761f56dffd277429",
+    "url": "/index.html"
+  }
+];
